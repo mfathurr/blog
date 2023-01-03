b32c948166e7a16f01ddd2a0141f898e966bfa8e
Muhammad Fathur Rachman <muhammad.fathur.rachman01@gmail.com>
2023-01-03 19:30:00 +0700

    Create Header component & hook header component to index

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
+<script>
+import Header from "./components/layouts/Header.vue";
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
 <template>
+  <Header></Header>
   <h1 class="text-3xl font-bold underline text-lime-400">
     Hello world!
   </h1>
