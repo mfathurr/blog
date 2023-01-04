fe3518cb0ac57129d919fb133f5ccbb94746352b
Muhammad Fathur Rachman <muhammad.fathur.rachman01@gmail.com>
2023-01-04 17:31:19 +0700

    Create MainContent component & hook MainContent component to index

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
 import Header from "./components/layouts/Header.vue";
+import MainContent from "./components/MainContent.vue";
 
 export default {
   components: {
-    Header
+    Header,
+    MainContent
   }
 }
 </script>
 
 <template>
   <Header></Header>
+  <MainContent></MainContent>
   <h1 class="text-3xl font-bold underline text-lime-400">
     Hello world!
   </h1>
