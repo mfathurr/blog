b6bb1db095f3ead49ce5115db25158b413323cb9
Muhammad Fathur Rachman <muhammad.fathur.rachman01@gmail.com>
2023-01-06 02:44:24 +0700

    Change image & alt text in MainContent, fixed wrong css link path in index.html, remove hello world from app

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,6 @@ export default {
 <template>
   <Header></Header>
   <MainContent></MainContent>
-  <h1 class="text-3xl font-bold underline text-lime-400">
-    Hello world!
-  </h1>
 </template>
 
 <style scoped>
